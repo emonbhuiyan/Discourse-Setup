9aadd120448092ad8e49705bafec872674def1b1
Emon Bhuiyan <mdemonbhuiyan@live.com>
2025-03-30 04:37:32 +0200

    Minor

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ CATEGORIES = {
 }
 topics = [
 ]
+#Formatted occupation and URLs (No manual input here)
+FORMATTED_OCCUPATION = OCCUPATION.replace(" ", "")
+SITE_URL = f"https://www.get{FORMATTED_OCCUPATION.lower()}jobs.com"
+DISCOURSE_URL = f"https://forum.get{FORMATTED_OCCUPATION.lower()}jobs.com"
+SITE_TITLE = f"Get{FORMATTED_OCCUPATION}Jobs.com Forum"
+SITE_DESCRIPTION = f"A community for {OCCUPATION.lower()} to connect, share, and find jobs."
